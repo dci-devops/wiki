cfb102863a4bb8c5bca37b48c8ab48960965ed71
dci-devops <161580808+dci-devops@users.noreply.github.com>
2024-07-02 19:04:25 +0200

    deleting comments

--- docker-wiki-and-db.sh
+++ docker-wiki-and-db.sh
@@ -8,24 +8,24 @@ mkdir -p ~/wiki/config ~/wiki/data
 sudo docker network create internal-network
 admin_name=administrator                   
 admin_pass=passwort1234                    
-sudo docker run \                          
-    --name postgresdb \                    
-    --network internal-network \           
-    -p 5432:5432 \                         
-    -e POSTGRES_DB=mydatabase \            
-    -e POSTGRES_USER=${admin_name} \       
-    -e POSTGRES_PASSWORD=${admin_pass} \   
-    -d postgres                            
-sudo docker run \                          
-    --name wiki \                          
-    --network internal-network \           
-    -p 80:3000 \                           
-    -e DB_TYPE=postgres \                  
-    -e DB_HOST=postgresdb \                
-    -e DB_PORT=5432 \                      
-    -e DB_USER=${admin_name} \             
-    -e DB_PASS=${admin_pass} \             
-    -e DB_NAME=mydatabase \                
-    -v ~/wiki/config:/var/wiki/config \    
-    -v ~/wiki/data:/var/wiki/data \        
-    -d requarks/wiki:latest                
+sudo docker run \
+    --name postgresdb \
+    --network internal-network \
+    -p 5432:5432 \
+    -e POSTGRES_DB=mydatabase \
+    -e POSTGRES_USER=${admin_name} \
+    -e POSTGRES_PASSWORD=${admin_pass} \
+    -d postgres   
+sudo docker run \
+    --name wiki \
+    --network internal-network \
+    -p 80:3000 \
+    -e DB_TYPE=postgres \
+    -e DB_HOST=postgresdb \
+    -e DB_PORT=5432 \
+    -e DB_USER=${admin_name} \
+    -e DB_PASS=${admin_pass} \
+    -e DB_NAME=mydatabase \
+    -v ~/wiki/config:/var/wiki/config \
+    -v ~/wiki/data:/var/wiki/data \
+    -d requarks/wiki:latest        
